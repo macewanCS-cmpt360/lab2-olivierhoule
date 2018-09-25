b4ef96c5f85a8bd091115514039ee75f8c52d8e3
olivierhoule <houleo2@mymacewan.ca>
2018-09-25 17:45:10 -0600

    Add support for "background" process

--- L2/shell.c
+++ L2/shell.c
@@ -8,7 +8,7 @@ int main(void)
 	while (1) {
 		char *cmdline;
 		char *token = NULL;
-		int i, rc;
+		int i, rc, bg;
 		char *args[10];
 
 		cmdline = calloc(1, 1024);
@@ -16,7 +16,7 @@ int main(void)
 
 		printf("prompt> ");
 		fgets(cmdline, 1024, stdin);
-		fprintf(stderr, "[debug] cmdline = *%s*\n", cmdline);
+		//fprintf(stderr, "[debug] cmdline = *%s*\n", cmdline);
 
 		token = strtok(cmdline, "\n ");
 		while (token != NULL) {
@@ -25,16 +25,24 @@ int main(void)
 			token = strtok(NULL, "\n ");
 		}
 		args[i] = NULL;
+		bg = 0;
+
+		if (strcmp(args[i-1], "&") != 0) {
+			args[i-1] = NULL;
+			bg = 1;
+		}
 
 		rc = fork();
 
 		if (rc < 0) { // fork failed; exit
 			fprintf(stderr, "fork failed\n");
 			exit(1);
-		} else if (rc == 0) {
+		} else if (rc == 0) { // child
 			execvp(args[0], args);
-		} else {
-			wait(NULL);
+		} else { // parent
+			if (!bg) {
+				wait(NULL);
+			}
 		}
 
 		free(cmdline);
