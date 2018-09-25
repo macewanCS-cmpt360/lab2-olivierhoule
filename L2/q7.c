254ef2a1de773425771c8df79287d7945d0582b7
olivierhoule <houleo2@mymacewan.ca>
2018-09-25 16:05:14 -0600

    Create initial q7.c

---
+++ L2/q7.c
@@ -0,0 +1,25 @@
+/*
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+	int rc = fork();
+
+	if (rc < 0) { // fork failed; exit
+		fprintf(stderr, "fork failed\n");
+		exit(1);
+	} else if (rc == 0) { // child process
+		close(STDOUT_FILENO);
+		printf("hello\n");
+	} else { // parent process
+		wait(NULL);
+	}
+	return 0;
+}
